1ff81e500a438f92beba8f6233a2360cbb000eef
peter <ptmartinezsound@gmail.com>
2022-06-16 08:49:05 -0500

    finish POST

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
