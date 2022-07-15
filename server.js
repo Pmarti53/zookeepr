c2b73a5460a9c575fd66db21a208e2ca73afbf95
peter <ptmartinezsound@gmail.com>
2022-07-14 20:09:14 -0500

    fixed front end bugs

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-app.use(express.static('public'));
-const { animals } = require('./data/animals');
-const fs = require('fs');
-const path = require('path');
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
+
+// const { animals } = require('./data/animals');
+// const fs = require('fs');
+// const path = require('path');
 
 // app.get('/api/animals', (req, res) => {
 //     let results = animals;
@@ -61,7 +70,3 @@ const path = require('path');
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, './public/index.html'));
 // });
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
-});
